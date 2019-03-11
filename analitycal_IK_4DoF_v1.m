f8348b254e32198ca36070f19d34ca1138553db7
cecprove <47523654+cecprove@users.noreply.github.com>
2019-03-11 14:22:40 +0100

    Update analitycal_IK_4DoF_v1.m

--- analitycal_IK_4DoF_v1.m
+++ analitycal_IK_4DoF_v1.m
@@ -3,7 +3,7 @@ function Q=analitycal_IK_4DoF(p,theta,a1,a2,a3,a4)
 
 %Applicazione del problema cinematico inverso (Noti p e theta->si vuole ricavare Q)
 %& guarda esempio del manipolatore a 3gdl fatto in classe
-q4= deg2rad(57);
+q4= deg2rad(7);
 pw_x=p(1)-a3*cos(theta-q4)-a4*cos(theta);
 pw_y=p(2)-a3*sin(theta-q4)-a4*sin(theta);
 
@@ -14,7 +14,7 @@ s1=((a1+a2*c2)*pw_y-a2*s2*pw_x)/(pw_x^2+pw_y^2);
 c1=((a1+a2*c2)*pw_x+a2*s2*pw_y)/(pw_x^2+pw_y^2);
 
 Q=[];
-%& se non sono reali quella soluzione è da scartare
+%& se non sono reali quella soluzione Ã¨ da scartare
 if (isreal(s1) && isreal(s2) && isreal(c1) && isreal(c2))
     q1=atan2(s1,c1);
     q2=atan2(s2,c2);
@@ -22,7 +22,7 @@ if (isreal(s1) && isreal(s2) && isreal(c1) && isreal(c2))
     %& ho una configurazione di angoli noti fi,theta e voglio
     %ricavare theta 1,2,3 ossia gli angoli di giunto. Se conosco tutto devo
     %riapplicare la cinematica diretta per vedere se la posizione che ho
-    %trovato siano quelle desiderati poichè questi algoritmi possono creare
+    %trovato siano quelle desiderati poichÃ¨ questi algoritmi possono creare
     %degli errori e quindi impongo che la differenza sia inferiore a e-3
     X = direct_kinematics_4DoF(q1,q2,q3,q4,a1,a2,a3,a4);%viene applicata la cinematica diretta
     
