ea7d5fa666562806fe62e45b69d6786b893b40cb
cmcaruso <camillocaruso95@gmail.com>
2019-03-11 12:37:04 +0100

    Update links_dimentioning_progetto.m

--- links_dimentioning_progetto.m
+++ links_dimentioning_progetto.m
@@ -22,7 +22,7 @@ joint_lim=[deg2rad(-90) deg2rad(90);
     deg2rad(-90) deg2rad(90)
     deg2rad(-90) deg2rad(90)];
 % &contiene un insieme di angoli e rappresentano gli estremi inferiore e
-%superiore oltre il quale il giunto non può muoversi. Sono epsressi in
+%superiore oltre il quale il giunto non puÃ² muoversi. Sono epsressi in
 %radianti
 
 %Traiettoria di esempio nello spazio operativo
@@ -53,11 +53,11 @@ link_lim=[1 20;
 
 %Range di variazione dei link nel metodo di ottimizzazione
 resolution=0.5;
-%& corrisponde al valore minimo che l'algorimo andrà a testare in base al
+%& corrisponde al valore minimo che l'algorimo andrÃ  a testare in base al
 %workspace che ho. Se non va bene la combinazione va scartata. per
 %scegliere quale tra le varie combinazione utilizzo una funzione di costo
 %come ad esempio quella che mi fa risprmiare materiale di stampa quindi la
-%somma dei tre lonk è minima ma non basta es uno da 1cm e altri molto
+%somma dei tre lonk Ã¨ minima ma non basta es uno da 1cm e altri molto
 %grandi--> sconsigliata. Quindi media dei tre link e poi vedo quanto ci si
 %discosta da questa media
 
@@ -69,27 +69,35 @@ links_=[];
 
 %3 cicli for innestati per scorrere i vettori
 %& i tre indici del for sono la lunghezza dei tre link e li faccio variare
-%tra il minimo e il massimo con un passo che è pari alla risoluzione
+%tra il minimo e il massimo con un passo che Ã¨ pari alla risoluzione
+conta_iterazioni = 0;
+iterazioni_tot = 1;
+for k = 1 : 4
+    iterazioni_k = ((link_lim(k,2) - link_lim(k,1))/resolution) + 1;
+    iterazioni_tot = iterazioni_tot * iterazioni_k;
+end
+
+barra = waitbar(0, 'Name', 'Barra di caricamento');
 for a1=link_lim(1,1):resolution:link_lim(1,2)
     for a2=link_lim(2,1):resolution:link_lim(2,2)
         for a3=link_lim(3,1):resolution:link_lim(3,2)
             for a4=link_lim(4,1):resolution:link_lim(4,2)
-                
-                
+                progress = conta_iterazioni/iterazioni_tot;
+                waitbar(progress, barra, sprintf("Running %.1f%%",progress));
                 
                 check=check_links_dimensions_4Dof(p,theta,a1,a2,a3,a4,joint_lim);
             
 
                 %Ciclo if
-                %& se check è vero inserisco in link a1,a2,a3 e se voglio
-                %conservare anche quelli precedenti (dove c'è il check vero)
+                %& se check Ã¨ vero inserisco in link a1,a2,a3 e se voglio
+                %conservare anche quelli precedenti (dove c'Ã¨ il check vero)
                 %scrivo links=[links; a1...]
                 if(check)
                     links=[links; a1, a2, a3, a4];
                     %& devo poi realizzare quella funzione che mi va a
                     % minimizzare quelle funzioni di costo che ho scelto prima
 
-                    %& prima funzione di costo è la somma dei link quindi creo
+                    %& prima funzione di costo Ã¨ la somma dei link quindi creo
                     %una variabile contenente le somme di tutti i link
                     links_sum=[links_sum;a1+a2+a3+a4];
                     %& mi permettono di trovare il massimo delle combinazioni
@@ -98,7 +106,7 @@ for a1=link_lim(1,1):resolution:link_lim(1,2)
 
                     links_diff=[links_diff;max_1234];
                 end
-                %& c'è _ .Qui metto tutte le combinazioni che ho trovato anche
+                %& c'Ã¨ _ .Qui metto tutte le combinazioni che ho trovato anche
                 %quelle che non vanno bene
                 links_=[links_; a1, a2, a3, a4];
             end
@@ -131,8 +139,8 @@ hold on
 plot(links_diff_norm,'-b','LineWidth',3)
 
 %& ho una funzione di costo la voglio minimizzare quindi prendo il minimo.
-%La tilde è un tool di matlab mi dice che voglio solo l'indice del valore
-%della funzione, stA PER non considerare tutto ciò che sta prima poichè min
+%La tilde Ã¨ un tool di matlab mi dice che voglio solo l'indice del valore
+%della funzione, stA PER non considerare tutto ciÃ² che sta prima poichÃ¨ min
 %fornisce sia il valore minimo che l'indice e noi gli diciamo di prendere
 %solo l'indice.
 [~, correct_ind] = min(cost_function);
