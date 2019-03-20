0c55444bb4d4583aadbcc97770814be995de2e3e
cecprove <47523654+cecprove@users.noreply.github.com>
2019-03-20 09:40:20 +0100

    Add files via upload

--- provatraiettoria.m
+++ provatraiettoria.m
@@ -12,7 +12,30 @@ for i=1:length(t)
     u(8)=t(i);
     [xd(i).traiettoria,xd(i).derivata]=planner_CACIOTTA(u,c1,c2,r1,r2);
 end
+
 for i=1:length(t)
 x(i)=xd(i).traiettoria(1);
 y(i)=xd(i).traiettoria(2);
+end
+plot(x',y')
+% per l'orientamento
+
+for i=1:length(t)
+a(i)=phi(i).orientamento;
+end
+plot(t',a')
+
+
+%% Prova con toad
+t=(0:0.1:20);
+u=[0.2 0.2 0.3 0.2 0 10 20 t 0 pi/6]; 
+% per come è scritto il codice dovremmo invertire i pedici
+c1=[0.25 0.495];
+c2=[0.25 0.11];
+r1=0.3;
+r2=0.1;
+for i=1:length(t)
+    u(10)=t(i);
+    [xd(i).traiettoria,xd(i).derivata,phi(i).orientamento,phi(i).derivata]=...
+        planner_TOAD(u,c1,c2,r1,r2);
 end
