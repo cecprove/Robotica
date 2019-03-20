8281389f30e9445c137cc83393038a9b037d0e06
leoloff96 <47523916+leoloff96@users.noreply.github.com>
2019-03-20 14:54:31 +0100

    Add files via upload

---
+++ initialization_simulazione.m
@@ -0,0 +1,7 @@
+%% Per far partire la simulazione
+u=[20 20 30 20 0 10 20 0 pi/6 0]; 
+% per come è scritto il codice dovremmo invertire i pedici
+c1=[25 49.5]; %centro circonferenza
+c2=[25 11]; %centro circonferenza
+r1=30; %raggio [cm]
+r2=10; %raggio [cm]
